ab88446dc71d5b7e432b6a78396e573c104d9990
ommanoj88 <127044672+ommanoj88@users.noreply.github.com>
2025-06-13 21:51:32 +0530

    Add files via upload

---
+++ src/main/java/com/tcrs/tcrs_backend/TcrsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tcrs.tcrs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcrsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcrsBackendApplication.class, args);
+	}
+
+}
